cf5ff5c05cb3b9d4f06bb4668de671fb8e1d2b40
Milos Spasojevic <milos.s.pfc@gmail.com>
2024-03-20 23:31:37 +0100

    turn on hearts

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,12 +21,12 @@ export default function RootLayout({
       // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
       // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
       // starting from v2 you can add only the features you need reducing the bundle size
-      // await loadAll(engine);
-      //await loadFull(engine);
+      await loadAll(engine);
+      // await loadFull(engine);
       // await loadSlim(engine);
       //await loadBasic(engine);
     }).then(() => {
-      // setInit(true);
+      setInit(true);
     });
   }, []);
 
